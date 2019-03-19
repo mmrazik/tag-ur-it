7cac6e351d05b379c155da6ffd213912d8cb3faa
Bryan MacFarlane <bryanmac@microsoft.com>
2019-03-19 18:05:48 -0400

    check for no issue-rules.yml

--- src/index.ts
+++ src/index.ts
@@ -15,34 +15,39 @@ export = (app: Application) => {
       let fileInfo = context.issue({ path: 'issue-rules.yml'});
       let fileContents = await context.github.repos.getContents(fileInfo);
   
-      // TODO: replace with Buffer.alloc
-      let buff = new Buffer(fileContents.data.content, fileContents.data.encoding); 
-      let issueRules: irm.IIssueRules = irm.parseYamlContents(buff.toString());
-      let eng: irm.RuleEngine = new irm.RuleEngine();
-    
-      console.log("processing rules");
-      let results: irm.ITagResults = eng.processRules(context.payload.issue.body, issueRules.rules);
-      console.log('results:')
-      console.log(results);
-
-      if (results.labelsToAdd && results.labelsToAdd.length == 0) {
-        results = eng.processRules(context.payload.issue.body, issueRules.noMatches);
-        console.log('results:')
-        console.log(results);        
-      }
-  
-      eng.processTags(results.labelsToAdd, issueRules.tags); 
-      console.log("tagsToAdd");
-      console.log(results.labelsToAdd);   
-  
-      const labels = context.issue({labels:results.labelsToAdd});
-      let res = await context.github.issues.addLabels(labels);
-
-      if (results.assigneesToAdd && results.assigneesToAdd.length > 0) {
-        const assignees = context.issue({assignees:results.assigneesToAdd});
-        let assignRes = await context.github.issues.addAssignees(assignees);
-      } 
+      if (fileContents) {
+        // TODO: replace with Buffer.alloc
+        let buff = new Buffer(fileContents.data.content, fileContents.data.encoding); 
+        let issueRules: irm.IIssueRules = irm.parseYamlContents(buff.toString());
+        let eng: irm.RuleEngine = new irm.RuleEngine();
       
+        console.log("processing rules");
+        let results: irm.ITagResults = eng.processRules(context.payload.issue.body, issueRules.rules);
+        console.log('results:')
+        console.log(results);
+
+        if (results.labelsToAdd && results.labelsToAdd.length == 0) {
+          results = eng.processRules(context.payload.issue.body, issueRules.noMatches);
+          console.log('results:')
+          console.log(results);        
+        }
+    
+        eng.processTags(results.labelsToAdd, issueRules.tags); 
+        console.log("tagsToAdd");
+        console.log(results.labelsToAdd);   
+    
+        const labels = context.issue({labels:results.labelsToAdd});
+        let res = await context.github.issues.addLabels(labels);
+
+        if (results.assigneesToAdd && results.assigneesToAdd.length > 0) {
+          const assignees = context.issue({assignees:results.assigneesToAdd});
+          let assignRes = await context.github.issues.addAssignees(assignees);
+        } 
+      }
+      else {
+        console.log("no issue-rules.yml file");
+      }
+
       const issueComment = context.issue({ body: 'Thanks for opening this issue!!' })
       await context.github.issues.createComment(issueComment)
     }
